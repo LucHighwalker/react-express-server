111ae697eec4ef63aaa67c66ca6819e07d2cb18e
LucHighwalker <luc.highwalker@gmail.com>
2019-02-07 17:04:54 -0800

    - Done

--- server.js
+++ server.js
@@ -1,36 +1,58 @@
 /* eslint-disable semi */
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const { random, randomD, randomRolls } = require('./utils')
+const {
+  random, randomD, randomRolls, lotto,
+} = require('./utils');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // ** Proxy from React can't get at '/' for some reason?
 // Apparently this is expected behavior... **
 // Test this route with: localhost:4000/
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' })
-})
+  res.json({ message: 'Hello World' });
+});
 
 // A simple route that returns a JSON object
 // Test this route with:
 app.get('/about', (req, res) => {
   // This Object is converted to JSON and returned.
-  res.json({ about: 'this service generates a random numbers.' })
-})
+  res.json({ about: 'this service generates a random numbers.' });
+});
 
 // Random number route
 // Test this route with: http://localhost:4000/random?n=99
 // Where n=99 sets the range of the random number returned
-app.get('/random', (req, res) => {
-  const { n } = req.query
-  const value = random(n)
-  res.json({ value })
-})
+app.get('/random/:number', (req, res) => {
+  const { number } = req.params;
+  const value = random(number);
+  res.json({ value });
+});
 
-const port = 4000
-app.listen(port, () => console.log(`LISTENING ON PORT ${port}`))
+app.get('/random/die/:sides', (req, res) => {
+  const { sides } = req.params;
+  const value = randomD(sides);
+  res.json({ value });
+});
+
+app.get('/random/dice/:sides/:rolls', (req, res) => {
+  const { sides, rolls } = req.params;
+  const value = randomRolls(sides, rolls);
+  res.json({ value });
+});
+
+app.get('/lotto', (req, res) => {
+  const lotNums = lotto();
+  res.json({
+    numbers: lotNums[0],
+    power: lotNums[1],
+  });
+});
+
+const port = 4000;
+app.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
--- utils.js
+++ utils.js
@@ -1,19 +1,34 @@
 /* eslint-disable semi */
 
-function random(n) {
-  return Math.random() * n
+function random(number) {
+  return Math.floor(Math.random() * number);
 }
 
-function randomD(n) {
-  
-  return 0
+function randomDie(sides) {
+  const min = Math.ceil(1);
+  const max = Math.floor(sides);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomRolls(n, s) {
-
-  return []
+function randomRolls(sides, rolls) {
+  const result = [];
+  for (let i = 0; i < rolls; i += 1) {
+    result.push(randomDie(sides));
+  }
+  return result;
 }
 
-module.exports.random = random
-module.exports.randomD = randomD
-module.exports.randomRolls = randomRolls
+function lotto() {
+  const result = [];
+  for (let i = 0; i < 5; i += 1) {
+    result.push(random(100));
+  }
+  return [result, random(100)];
+}
+
+module.exports = {
+  random,
+  randomDie,
+  randomRolls,
+  lotto,
+};
